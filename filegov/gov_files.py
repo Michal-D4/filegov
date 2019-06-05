96fb7e7d2420b92f798af84d8f8f9f1a9b1c3328
Michal <mihal.d44@gmail.com>
2019-06-05 08:34:23 +0300

    import . (is '.' need for curr.dir?)

--- filegov/gov_files.py
+++ filegov/gov_files.py
@@ -12,18 +12,18 @@ from PyQt5.QtCore import (Qt, QModelIndex, QItemSelectionModel, QSettings, QDate
 from PyQt5.QtWidgets import (QInputDialog, QLineEdit, QFileDialog, QLabel,
                              QFontDialog, QApplication, QMessageBox)
 
-from table_model import TableModel, ProxyModel2
-from tree_model import TreeModel
-from edit_tree_model import EditTreeModel, EditTreeItem
-from file_info import FileInfo, LoadFiles
-from helper import (EXT_ID_INCREMENT, Fields, Shared, show_message)
-from utilities import DBUtils
-import create_db
-from load_db_data import LoadDBData
-from input_date import DateInputDialog
-from item_edit import ItemEdit
-from sel_opt import SelOpt
-from set_fields import SetFields
+from .table_model import TableModel, ProxyModel2
+from .tree_model import TreeModel
+from .edit_tree_model import EditTreeModel, EditTreeItem
+from .file_info import FileInfo, LoadFiles
+from .helper import (EXT_ID_INCREMENT, Fields, Shared, show_message)
+from .utilities import DBUtils
+from .create_db import create_all_objects
+from .load_db_data import LoadDBData
+from .input_date import DateInputDialog
+from .item_edit import ItemEdit
+from .sel_opt import SelOpt
+from .set_fields import SetFields
 
 
 DETECT_TYPES = sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES
@@ -375,7 +375,7 @@ class FilesCrt():
         if create:
             _connection = sqlite3.connect(file_name, check_same_thread=False,
                                           detect_types=DETECT_TYPES)
-            create_db.create_all_objects(_connection)
+            create_all_objects(_connection)
         else:
             if os.path.isfile(file_name):
                 _connection = sqlite3.connect(file_name, check_same_thread=False,
